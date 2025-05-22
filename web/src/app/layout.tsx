5ef1c6a8151d75dc5dee4f541cafe5bd39fc1e0b
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-05-21 21:32:56 -0300

    feat: add toast provider

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { ToastProvider } from "@/components/common/ToastProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,8 @@ export default function RootLayout({
         className={`${geistSans.variable} antialiased`}
       >
         {children}
+
+        <ToastProvider />
       </body>
     </html>
   );
