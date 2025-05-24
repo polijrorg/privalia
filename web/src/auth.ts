28c43098f13e54b3af997236af0a866db90d8b90
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-05-23 21:58:26 -0300

    feat: add basic roles to user

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -3,8 +3,21 @@ import Google from "next-auth/providers/google"
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/services/prisma";
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.role = user.role ?? 'USER';
+        token.id = user.id;
+      };
+      return token
+    },
+    session({ session, token }) {
+      session.user.role = token.role as string ?? 'user';
+      return session
+    }
+  }
 })
