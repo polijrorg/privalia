0baad425ab1747533f7917d873c30fa8e71eaaaf
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-06-07 19:10:44 -0300

    chore: rm webpack svgr (redundant now apparently)

--- web/next.config.ts
+++ web/next.config.ts
@@ -9,13 +9,6 @@ const nextConfig: NextConfig = {
       },
     },
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
   async rewrites() {
     return [
       {
