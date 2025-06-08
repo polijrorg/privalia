226e9151f0d1f3d98d3c8fc42564c38ccf5005b9
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-06-07 22:13:50 -0300

    tests: add user post test & simplify create request logic

--- web/tests/integration/api/materia/index.test.ts
+++ web/tests/integration/api/materia/index.test.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect, vi, beforeEach, Mock } from 'vitest'
 import * as materiaService from '@/backend/services/materia'
 import { GET, POST } from '@/backend/api/materia/route'
-import { NextRequest } from 'next/server'
 import { getMateriasMock, postMateriaMock } from '../../mocks/materia'
 import { setCurrentRole } from '../../mocks/auth'
+import { createRequest } from '../../mocks/requests'
 
 vi.mock('@/backend/services/materia', () => ({
   getAllMaterias: vi.fn(),
@@ -33,26 +33,17 @@ describe('POST /api/materia', () => {
     setCurrentRole(null); // Reset auth context
   });
 
-  const createRequest = (body = postMateriaMock) => {
-    const headers = new Headers();
-    headers.set('Content-Type', 'application/json');
-
-    return new NextRequest('http://localhost:3000/api/materia', {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers,
-    });
-  };
+  const createMateriaRequest = () => createRequest(postMateriaMock, "materia")
 
   it('should fail if unauthenticated', async () => {
     setCurrentRole(null);
-    const response = await POST(createRequest());
+    const response = await POST(createMateriaRequest());
     expect(response?.status).toBe(401);
   });
 
   it('should fail if user is not ADMIN or SUPER_ADMIN', async () => {
     setCurrentRole('USER');
-    const response = await POST(createRequest());
+    const response = await POST(createMateriaRequest());
     expect(response?.status).toBe(403);
   });
 
@@ -60,7 +51,7 @@ describe('POST /api/materia', () => {
     setCurrentRole('ADMIN');
     (materiaService.createMateria as Mock).mockResolvedValue(postMateriaMock);
 
-    const response = await POST(createRequest());
+    const response = await POST(createMateriaRequest());
     expect(response?.status).toBe(201);
     
     const data = await response?.json();
@@ -72,10 +63,11 @@ describe('POST /api/materia', () => {
     setCurrentRole('SUPER_ADMIN');
     (materiaService.createMateria as Mock).mockResolvedValue(postMateriaMock);
 
-    const response = await POST(createRequest());
+    const response = await POST(createMateriaRequest());
     expect(response?.status).toBe(201);
     
     const data = await response?.json();
     expect(data).toEqual(postMateriaMock);
+    expect(materiaService.createMateria).toHaveBeenCalledWith(postMateriaMock);
   });
 });
---
+++ web/tests/integration/api/user/index.test.ts
@@ -0,0 +1,43 @@
+import { describe, it, vi, beforeEach, expect, Mock } from "vitest";
+import * as userService from '@/backend/services/user'
+import * as authService from '@/backend/services/auth'
+import { postUserMock } from "../../mocks/user";
+import { createRequest } from "../../mocks/requests";
+
+vi.mock('@/backend/services/user', () => ({
+  findUserByEmail: vi.fn(),
+  createUser: vi.fn(),
+}))
+
+vi.mock('@/backend/services/auth', () => ({
+  saltAndHashPassword: vi.fn()
+}))
+
+import { POST } from '@/backend/api/user/route'
+
+describe('POST /api/user', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const createUserRequest = () => createRequest(postUserMock, 'user')
+
+  it("should fail if there's another user with the same email", async () => {
+    (userService.findUserByEmail as Mock).mockResolvedValue(postUserMock);
+
+    const response = await POST(createUserRequest());
+    expect(response?.status).toBe(409);
+  });
+
+  it('should register if everything is alright', async () => {
+    (userService.findUserByEmail as Mock).mockResolvedValue(null);
+    (userService.createUser as Mock).mockResolvedValue(postUserMock);
+    (authService.saltAndHashPassword as Mock).mockResolvedValue("HashedPassword");
+
+    const response = await POST(createUserRequest());
+    expect(response?.status).toBe(201);
+    
+    const data = await response?.json();
+    expect(data.user).toBeTruthy();
+  });
+})
