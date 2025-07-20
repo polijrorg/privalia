273b6d53f0bc394ea93d36f3b2eb834b9cd3bd57
Victor Lima de Oliveira <victorlima.oliveira70@gmail.com>
2025-07-20 09:44:46 -0300

    fix: Esqueci de subir dois arquivos

--- mobile/components/Button.tsx
+++ mobile/components/Button.tsx
@@ -1,24 +1,43 @@
 import { forwardRef } from 'react';
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import { ButtonType } from '~/types/ButtonTypes';
 
 type ButtonProps = {
   title: string;
+  variant?: ButtonType;
 } & TouchableOpacityProps;
 
-export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps }, ref) => {
-  return (
-    <TouchableOpacity
-      ref={ref}
-      {...touchableProps}
-      className={`${styles.button} ${touchableProps.className}`}>
-      <Text className={styles.buttonText}>{title}</Text>
-    </TouchableOpacity>
-  );
-});
+export const Button = forwardRef<View, ButtonProps>(
+  ({ title, variant = ButtonType.Filled, ...touchableProps }, ref) => {
+    let containerClass = '';
+    let textClass = '';
+
+    switch (variant) {
+      case ButtonType.Text:
+        containerClass = 'bg-transparent';
+        textClass = 'text-primary';
+        break;
+
+      case ButtonType.Outlined:
+        containerClass = 'border border-primary bg-transparent';
+        textClass = 'text-primary';
+        break;
+
+      case ButtonType.Filled:
+      default:
+        containerClass = 'bg-primary';
+        textClass = 'text-white';
+        break;
+    }
+    return (
+      <TouchableOpacity
+        ref={ref}
+        {...touchableProps}
+        className={`items-center rounded-xl p-4 shadow-md ${containerClass}`}>
+        <Text className={`text-center text-lg font-semibold ${textClass}`}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+);
 
 Button.displayName = 'Button';
-
-const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
-  buttonText: 'text-white text-lg font-semibold text-center',
-};
