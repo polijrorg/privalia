8c3be95d41bd80ea90b37f919d76491c1eddcd4f
Flavio <flaviodccremaschi@gmail.com>
2025-08-06 08:47:48 -0300

    wip: Crud feita mas não funciona

--- web/src/app/(backend)/api/item/[auditoriaId]/[itemId]/route.ts
+++ web/src/app/(backend)/api/item/[auditoriaId]/[itemId]/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { returnInvalidDataErrors, validBody, zodErrorHandler } from '@/utils';
 import { auth } from '@/auth';
-import { findAuditoriaById } from '../../../services/auditoria';
-import { createItem, deleteItem, getItems, updateItem } from '../../../services/item';
-import { createItemSchema, patchItemSchema } from '../../../schemas/item.schema';
+import { findAuditoriaById } from '../../../../services/auditoria';
+import { createItem, deleteItem, getItems, updateItem } from '../../../../services/item';
+import { createItemSchema, patchItemSchema } from '../../../../schemas/item.schema';
 
 export async function DELETE(request: NextRequest, { params }: { params: { itemId: string } }) {
   try {
