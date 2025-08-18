31c1d182cb228cbd24aeeb5bfa34a2f7c7f3a09e
Flavio <flaviodccremaschi@gmail.com>
2025-08-18 13:05:39 -0300

    Ajuste do schema

--- web/src/app/(backend)/schemas/auditoria.schema.ts
+++ web/src/app/(backend)/schemas/auditoria.schema.ts
@@ -6,10 +6,12 @@ export const createAuditoriaSchema = z.object({
   categoria: z.string().min(2, 'Categoria obrigatória'),
   amostra: z.number(),
   total_pecas: z.number(),
-  PO: z.string(),
+  PO: z.string().nullable(),
   fornecedor: z.string(),
   status: z.string(),
-  idExterno: z.string()
+  idExterno: z.string().nullable(),
+  modelo_negocio: z.string(),
+  pedido: z.string().nullable(),
 });
 
 export const patchAuditoriaSchema = createAuditoriaSchema.partial().refine((obj) => Object.keys(obj).length > 0, {
